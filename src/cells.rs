d98117b5d2cc37812076a86d79f42a4e09aa4c7a
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-24 00:31:52 -0700

    cleanup

--- src/cells.rs
+++ src/cells.rs
@@ -32,35 +32,33 @@ pub struct CellsTimer(Timer);
 
 pub fn init_cells(mut commands: Commands) {
   commands.spawn(Camera2dBundle::default());
-  (0..DEFAULT_CELLS_GRID_SIZE)
-    .for_each(|i| {
-      (0..DEFAULT_CELLS_GRID_SIZE)
-        .for_each(|j| {
-          let cell_state = if rand::random() {
-            CellState::Alive
-          } else {
-            CellState::Dead
-          };
-          let color = if cell_state == CellState::Alive { Color::WHITE } else { Color::BLACK };
-          let sprite = SpriteBundle {
-            sprite: Sprite {
-              color,
-              custom_size: Some(Vec2::new(DEFAULT_CELL_SIZE, DEFAULT_CELL_SIZE)),
-              ..default()
-            },
-            transform: Transform::from_xyz( 
-              (j as f32 * DEFAULT_CELL_SIZE) + (DEFAULT_CELL_SIZE / 2.) - (WINDOW_SIZE / 2.), 
-              (i as f32 * DEFAULT_CELL_SIZE) + (DEFAULT_CELL_SIZE / 2.) - (WINDOW_SIZE / 2.), 
-              0.
-            ),
-            ..default()
-          };
-          commands.spawn(CellBundle {
-            cell_state,
-            sprite,
-          });
-        });
+  (0..DEFAULT_CELLS_GRID_SIZE).for_each(|i| {
+    (0..DEFAULT_CELLS_GRID_SIZE).for_each(|j| {
+      let cell_state = if rand::random() {
+        CellState::Alive
+      } else {
+        CellState::Dead
+      };
+      let color = if cell_state == CellState::Alive { Color::WHITE } else { Color::BLACK };
+      let sprite = SpriteBundle {
+        sprite: Sprite {
+          color,
+          custom_size: Some(Vec2::new(DEFAULT_CELL_SIZE, DEFAULT_CELL_SIZE)),
+          ..default()
+        },
+        transform: Transform::from_xyz( 
+          (j as f32 * DEFAULT_CELL_SIZE) + (DEFAULT_CELL_SIZE / 2.) - (WINDOW_SIZE / 2.), 
+          (i as f32 * DEFAULT_CELL_SIZE) + (DEFAULT_CELL_SIZE / 2.) - (WINDOW_SIZE / 2.), 
+          0.
+        ),
+        ..default()
+      };
+      commands.spawn(CellBundle {
+        cell_state,
+        sprite,
+      });
     });
+  });
 }
 
 pub fn update_grid(
@@ -89,16 +87,14 @@ const DIRECTIONS: [(isize, isize); 8] = [
 
 fn next_cell_states(i: usize, j: usize, cell_states: &Vec<CellState>) -> CellState {
   let mut alive_neighbor_count: u8 = 0;
-  DIRECTIONS
-    .iter()
-    .for_each(|(i_off, j_off)| {
-      let i = ((i as isize + i_off + DEFAULT_CELLS_GRID_SIZE as isize) % DEFAULT_CELLS_GRID_SIZE as isize) as usize;
-      let j = ((j as isize + j_off + DEFAULT_CELLS_GRID_SIZE as isize) % DEFAULT_CELLS_GRID_SIZE as isize) as usize;
-      let neighbor_cell_state = cell_states[(i * DEFAULT_CELLS_GRID_SIZE) + j].clone();
-      if neighbor_cell_state == CellState::Alive { 
-        alive_neighbor_count += 1;
-      }
-    });
+  DIRECTIONS.iter().for_each(|(i_off, j_off)| {
+    let i = ((i as isize + i_off + DEFAULT_CELLS_GRID_SIZE as isize) % DEFAULT_CELLS_GRID_SIZE as isize) as usize;
+    let j = ((j as isize + j_off + DEFAULT_CELLS_GRID_SIZE as isize) % DEFAULT_CELLS_GRID_SIZE as isize) as usize;
+    let neighbor_cell_state = cell_states[(i * DEFAULT_CELLS_GRID_SIZE) + j].clone();
+    if neighbor_cell_state == CellState::Alive { 
+      alive_neighbor_count += 1;
+    }
+  });
   
   if alive_neighbor_count <= 1 || alive_neighbor_count >= 4 {
     CellState::Dead
